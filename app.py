a4228908577123036ed8d8d323299b535287a5d5
Armand3125 <armandjulien05@gmail.com>
2024-11-07 15:00:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import streamlit as st
 
 # Titre de l'application
-st.title('Exemple avec trois boutons')
+st.title('Exemple avec trois boutons sur la même ligne')
 
-# Boutons
-if st.button('Bouton 1'):
-    st.write('Vous avez cliqué sur le Bouton 1')
-    
-if st.button('Bouton 2'):
-    st.write('Vous avez cliqué sur le Bouton 2')
-    
-if st.button('Bouton 3'):
-    st.write('Vous avez cliqué sur le Bouton 3')
+# Créer 3 colonnes pour aligner les boutons
+col1, col2, col3 = st.columns(3)
+
+# Boutons dans chaque colonne
+with col1:
+    if st.button('Bouton 1'):
+        st.write('Vous avez cliqué sur le Bouton 1')
+
+with col2:
+    if st.button('Bouton 2'):
+        st.write('Vous avez cliqué sur le Bouton 2')
+
+with col3:
+    if st.button('Bouton 3'):
+        st.write('Vous avez cliqué sur le Bouton 3')
