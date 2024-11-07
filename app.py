dfe2ad6b32cd97a0f1514ce6696a6bfdc5bcd5b2
Armand3125 <armandjulien05@gmail.com>
2024-11-07 15:02:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Titre de l'application
-st.title('Exemple avec trois boutons et trois zones de couleur encadrées')
+st.title('Exemple avec trois boutons dans les zones de couleur')
 
 # Créer 3 colonnes pour aligner les boutons
 col1, col2, col3 = st.columns(3)
@@ -13,14 +13,20 @@ selected_color = None
 with col1:
     if st.button('Bouton 1'):
         selected_color = 'red'
-    st.markdown('<div style="height:100px; background-color:red; border: 2px solid {}; margin-top: 10px;"></div>'.format('black' if selected_color == 'red' else 'transparent'), unsafe_allow_html=True)
+    st.markdown('<div style="height:100px; background-color:red; border: 2px solid {}; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
+                '<button style="background-color:transparent; border:none; color:white;">Bouton 1</button>'
+                '</div>'.format('black' if selected_color == 'red' else 'transparent'), unsafe_allow_html=True)
 
 with col2:
     if st.button('Bouton 2'):
         selected_color = 'blue'
-    st.markdown('<div style="height:100px; background-color:blue; border: 2px solid {}; margin-top: 10px;"></div>'.format('black' if selected_color == 'blue' else 'transparent'), unsafe_allow_html=True)
+    st.markdown('<div style="height:100px; background-color:blue; border: 2px solid {}; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
+                '<button style="background-color:transparent; border:none; color:white;">Bouton 2</button>'
+                '</div>'.format('black' if selected_color == 'blue' else 'transparent'), unsafe_allow_html=True)
 
 with col3:
     if st.button('Bouton 3'):
         selected_color = 'green'
-    st.markdown('<div style="height:100px; background-color:green; border: 2px solid {}; margin-top: 10px;"></div>'.format('black' if selected_color == 'green' else 'transparent'), unsafe_allow_html=True)
+    st.markdown('<div style="height:100px; background-color:green; border: 2px solid {}; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
+                '<button style="background-color:transparent; border:none; color:white;">Bouton 3</button>'
+                '</div>'.format('black' if selected_color == 'green' else 'transparent'), unsafe_allow_html=True)
