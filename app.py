f9f1185282b7728cc6c7496023e1000a73ea6037
Armand3125 <armandjulien05@gmail.com>
2024-11-07 15:07:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,31 +11,25 @@ selected_color = None
 
 # Boutons et zones de couleur dans chaque colonne
 with col1:
-    # Création du bouton dans le carré coloré
     if st.button('Bouton 1'):
         selected_color = 'red'
     st.markdown(
         f'<div style="height:100px; background-color:red; border: 2px solid { "black" if selected_color == "red" else "transparent" }; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
-        '<button style="background-color:transparent; border:none; color:white; font-size:18px; cursor:pointer;">Bouton 1</button>'
         '</div>',
         unsafe_allow_html=True)
 
 with col2:
-    # Création du bouton dans le carré coloré
     if st.button('Bouton 2'):
         selected_color = 'blue'
     st.markdown(
         f'<div style="height:100px; background-color:blue; border: 2px solid { "black" if selected_color == "blue" else "transparent" }; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
-        '<button style="background-color:black; border:none; color:white; font-size:18px; cursor:pointer;">Bouton 2</button>'
         '</div>',
         unsafe_allow_html=True)
 
 with col3:
-    # Création du bouton dans le carré coloré
     if st.button('Bouton 3'):
         selected_color = 'green'
     st.markdown(
         f'<div style="height:100px; background-color:green; border: 2px solid { "black" if selected_color == "green" else "transparent" }; display: flex; justify-content: center; align-items: center; margin-top: 10px;">'
-        '<button style="background-color:transparent; border:none; color:white; font-size:18px; cursor:pointer;">Bouton 3</button>'
         '</div>',
         unsafe_allow_html=True)
