22a05349864a196fa6261645ee7684ddf2ef3f04
Armand3125 <armandjulien05@gmail.com>
2024-11-07 14:58:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
-import random
+import streamlit as st
 
-app = Flask(__name__)
+# Titre de l'application
+st.title('Exemple avec trois boutons')
 
-@app.route('/')
-def home():
-    # Générer des couleurs aléatoires pour les boutons
-    colors = ['#FF6347', '#4682B4', '#32CD32']  # Rouge, Bleu, Vert
-    random.shuffle(colors)  # Mélanger les couleurs
-    return render_template('index.html', colors=colors)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Boutons
+if st.button('Bouton 1'):
+    st.write('Vous avez cliqué sur le Bouton 1')
+    
+if st.button('Bouton 2'):
+    st.write('Vous avez cliqué sur le Bouton 2')
+    
+if st.button('Bouton 3'):
+    st.write('Vous avez cliqué sur le Bouton 3')
