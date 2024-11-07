d8224fda3db369d321d139f486f89846d59aecdf
Armand3125 <armandjulien05@gmail.com>
2024-11-07 15:26:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import streamlit as st
 
-# Insérer du CSS pour personnaliser l'apparence du bouton
-st.markdown("""
+# Récupérer la couleur primaire définie dans le fichier config.toml
+primaryColor = st.get_option("theme.primaryColor")
+
+# Appliquer la couleur du thème au bouton via CSS
+st.markdown(f"""
 <style>
-div.stButton > button:first-child {
-    background-color: #0099ff;  /* couleur de fond du bouton */
-    color: #ffffff;  /* couleur du texte */
+div.stButton > button:first-child {{
+    background-color: {primaryColor};  /* couleur de fond du bouton */
+    color: white;  /* couleur du texte */
     font-size: 20px;  /* taille de la police */
     height: 3em;  /* hauteur du bouton */
     width: 15em;  /* largeur du bouton */
     border-radius: 10px;  /* arrondir les coins du bouton */
 }
 
-div.stButton > button:hover {
+div.stButton > button:hover {{
     background-color: #00ff00;  /* couleur de fond lorsque l'on survole le bouton */
     color: #ff0000;  /* couleur du texte lorsqu'on survole */
-}
+}}
 </style>
 """, unsafe_allow_html=True)
 
