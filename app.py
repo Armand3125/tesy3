0007abfdf9fc782c2d42c6f9a5f81ece3f7642c6
Armand3125 <armandjulien05@gmail.com>
2024-11-12 14:00:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,18 +40,26 @@ uploaded_image = st.file_uploader("Télécharger une image", type=["jpg", "jpeg"
 if "num_selections" not in st.session_state:
     st.session_state.num_selections = 4
 
-# Initialisation des couleurs sélectionnées
-selected_colors = []
+col1, col2 = st.columns([1, 5])
 
-# Affichage des cases de couleurs sans texte
+with col1:
+    if st.button("4 Couleurs"):
+        st.session_state.num_selections = 4
+
+with col2:
+    if st.button("6 Couleurs"):
+        st.session_state.num_selections = 6
+
 num_selections = st.session_state.num_selections
+
 rectangle_width = 80 if num_selections == 4 else 50
 rectangle_height = 20
 
 cols = st.columns(num_selections * 2)
 color_options = list(pal.keys())
 
-# Affichage des cases à cocher pour la sélection des couleurs
+# Affichage des cases de couleurs sans texte
+selected_colors = []
 for i in range(num_selections):
     with cols[i * 2]:
         st.markdown("<div class='color-container'>", unsafe_allow_html=True)
@@ -68,18 +76,6 @@ for i in range(num_selections):
         selected_color_name = st.radio("", color_options, key=f"radio_{i}")
         selected_colors.append(pal[selected_color_name])  # Enregistrer la couleur sélectionnée
 
-# Affichage du nombre de couleurs (4 ou 6)
-col1, col2 = st.columns([1, 5])
-
-with col1:
-    if st.button("4 Couleurs"):
-        st.session_state.num_selections = 4
-
-with col2:
-    if st.button("6 Couleurs"):
-        st.session_state.num_selections = 6
-
-# Traitement de l'image avec KMeans après la sélection de l'image
 if uploaded_image is not None:
     image = Image.open(uploaded_image)
     
@@ -94,13 +90,14 @@ if uploaded_image is not None:
     
     resized_image = image.resize((new_width, new_height))
 
-    # Traitement de l'image avec KMeans
+    # Traitement KMeans
     img_arr = np.array(resized_image)
     pixels = img_arr.reshape(-1, 3)
 
     # Appliquer KMeans pour le nombre de clusters en fonction de la sélection
     kmeans = KMeans(n_clusters=num_selections, random_state=0).fit(pixels)
     labels = kmeans.labels_
+    centers = kmeans.cluster_centers_
 
     # Remplacer les pixels par la couleur de leur cluster sélectionnée
     new_img_arr = np.zeros_like(img_arr)
@@ -112,5 +109,5 @@ if uploaded_image is not None:
     # Convertir l'image transformée en image PIL pour l'afficher
     new_image = Image.fromarray(new_img_arr.astype('uint8'))
 
-    # Afficher l'image après traitement KMeans en haut
+    # Afficher uniquement l'image après traitement KMeans
     st.image(new_image, caption=f"Image après traitement KMeans ({num_selections} couleurs)", use_column_width=False)
