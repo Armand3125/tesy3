6c19b3a7c96405ff129ad8d7df60603c7d64a38a
Armand3125 <armandjulien05@gmail.com>
2024-11-20 15:00:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ css = """
     <style>
         .stRadio div [data-testid="stMarkdownContainer"] p { display: none; }
         .radio-container { display: flex; flex-direction: column; align-items: center; margin: 0; }
-        .color-container { display: flex; flex-direction: column; align-items: center; margin-top: 5px; }
+        .color-container { display: flex; flex-direction: column; align-items: center; margin-top: 0px; }
         .color-box { border: 3px solid black; }
         .stColumn { padding: 0 !important; }
         .first-box { margin-top: 15px; }
