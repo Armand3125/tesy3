824a2489f585e158604b4c26f58757fa9abd7092
Armand3125 <armandjulien05@gmail.com>
2024-11-27 16:09:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,17 @@ with col2:
     if st.button("6 Couleurs : 11.95 €"):
         st.session_state.num_selections = 6
 
+# Section de conseils
+st.markdown("""
+    ### 📝 Conseils d'utilisation :
+    - **Les zones de couleurs sont données par ordre de présence dans l'image.**
+    - **L'ordre des couleurs est déterminé par leur proximité avec les couleurs d'origine de l'image.**
+    - Il est recommandé d'inclure au moins une zone de **noir** ou de **blanc** pour assurer un bon contraste.
+    - Utiliser des **familles de couleurs** (par exemple, blanc, jaune, orange, rouge) peut produire des résultats visuellement intéressants.
+    - Expérimentez avec différentes combinaisons pour trouver l'esthétique qui correspond le mieux à votre projet.
+""", unsafe_allow_html=True)
+
+
 num_selections = st.session_state.num_selections
 cols_percentages = st.columns(num_selections)
 
