714901559ce5ebd9a401bbc1b4d56cc68aed42c5
Armand3125 <armandjulien05@gmail.com>
2024-11-28 16:21:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,10 @@ st.markdown("""
     ### 📝 Conseils d'utilisation :
     - **Les couleurs les plus compatibles avec l'image apparaissent en premier.**
     - **Préférez des images avec un bon contraste et des éléments bien définis.**
-    - **Une image carrée donnera un meilleur résultat.**
-    - **Il est recommandé d'inclure au moins une zone de noir ou de blanc pour assurer un bon contraste.**
-    - **Utiliser des familles de couleurs (ex: blanc, jaune, orange, rouge) peut produire des résultats visuellement intéressants.**
-    - **Expérimentez avec différentes combinaisons pour trouver l'esthétique qui correspond le mieux à votre projet !**
+    - **Une image carrée** donnera un meilleur résultat.
+    - Il est recommandé d'inclure au moins **une zone de noir ou de blanc** pour assurer un bon contraste.
+    - Utiliser des **familles de couleurs** (ex: blanc, jaune, orange, rouge) peut produire des résultats visuellement intéressants.
+    - **Expérimentez** avec différentes combinaisons pour trouver l'esthétique qui correspond le mieux à votre projet !
 """, unsafe_allow_html=True)
 
 
