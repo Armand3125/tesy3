6855d202aa15d0d3d85f4d10d06f556211132153
Armand3125 <armandjulien05@gmail.com>
2024-11-28 16:18:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.markdown("""
     - **Les couleurs les plus compatibles avec l'image apparaissent en premier.**
     - **Préférez des images avec un bon contraste et des éléments bien définis.**
     - **Une image carrée donnera un meilleur résultat.**
-    - **Il est recommandé d'inclure du noir ou du blanc pour le contraste et d'utiliser des familles de couleurs (par exemple, blanc, jaune, orange, rouge) pour un rendu harmonieux.**
+    - **Il est recommandé d'inclure du noir ou du blanc pour le contraste et d'utiliser des familles de couleurs (ex: blanc, jaune, orange, rouge) pour un rendu harmonieux.**
     - **Expérimentez avec différentes combinaisons pour trouver l'esthétique qui correspond le mieux à votre projet !**
 """, unsafe_allow_html=True)
 
